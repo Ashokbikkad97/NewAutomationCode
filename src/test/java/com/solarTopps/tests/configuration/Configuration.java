fb01dc3da77cbae62096555b8e73442a47fe4c18
Ashokbikkad97 <bikkada97@gmail.com>
2023-12-11 14:38:53 +0530

    adding New Code

---
+++ src/test/java/com/solarTopps/tests/configuration/Configuration.java
@@ -0,0 +1,19 @@
+package com.solarTopps.tests.configuration;
+
+import com.solarTopps.tests.interfaces.Config;
+
+public class Configuration implements Config {
+
+	public static  String path = "D:\\Soft for EC\\chromedriver-win64\\chromedriver.exe";
+	public static  String driver = "webdriver.chrome.driver";
+	public static  String url = "https://dev.solartopps.com/";
+	public static  String urlqa = "https://demoqa.com/";
+	public static  String admin = "admin@texala.net";
+	public static  String dealer = "abikkad@texala.net"; 
+	public static  String ec = "aisha@texala.net";
+	public static  String order = "order@solartopps.com";
+	public static  String site = "site@mailinator.com";
+	public static  String pass = "Tex@1234";
+	public static  String AdminPass = "Admin*1234";
+	
+}
